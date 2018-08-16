6c8c04a708683a1bfb2be519ff3c7da90ae0c2b2
zorro <cyroon@163.com>
2018-08-16 12:30:04 +0800

    add: ping testcase

---
+++ handler/ping_test.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_ping(t *testing.T) {
+	type args struct {
+		req pingReq
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *pingResp
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{name: "t01", args: args{req: pingReq{}}},
+	}
+	for _, tt := range tests {
+		got, err := ping(tt.args.req)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("%q. ping() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			continue
+		}
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ping() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
